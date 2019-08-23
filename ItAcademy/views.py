9ffae756618b7fb94347fa5d19f1eaac5bbef238
Dmitriy Posypaiko <dima.posypajko@gmail.com>
2019-08-23 17:54:28 +0300

    Added html

---
+++ ItAcademy/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views.generic import ListView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+
+
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+
+def login(request):
+    context ={}
+    return render(request, 'login.html', context)
